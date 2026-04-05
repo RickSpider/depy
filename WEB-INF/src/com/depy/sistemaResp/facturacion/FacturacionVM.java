1599590aa790860f0c80f601fcf3c04375182849
RickSpider <rrgi89@hotmail.com>
2026-04-05 18:10:16 -0300

    Correcciones en responsive

--- WEB-INF/src/com/depy/sistemaResp/facturacion/FacturacionVM.java
+++ WEB-INF/src/com/depy/sistemaResp/facturacion/FacturacionVM.java
@@ -2,16 +2,22 @@ package com.depy.sistemaResp.facturacion;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Sessions;
 import org.zkoss.zk.ui.util.Clients;
 
+import com.depy.modelo.Empresa;
+import com.depy.modelo.Factura;
+import com.depy.modelo.FacturaDetalle;
 import com.doxacore.util.Register;
 import com.doxacore.util.UtilMetodos;
 
@@ -19,6 +25,8 @@ public class FacturacionVM{
 	
 	private UtilMetodos um;
 	private Register reg;
+	
+	private Factura facturaSelected;
 
 	private List<Object[]> facturaciones;
 	private List<Object[]> facturacionesOri;
@@ -27,6 +35,10 @@ public class FacturacionVM{
 	
 	private int rechazados;
 	private int aprobados;
+	
+	
+	
+	private Boolean[] pantalla = {true, false};
 	
 	@Init(superclass = true)
 	public void initFacturacionVM() {
@@ -145,6 +157,103 @@ public class FacturacionVM{
 		this.filtrarFacturas(this.filtro, this.btnfIdActual.replace("btnf", ""));
 		
 	}
+	
+	@Command
+	@NotifyChange({"pantalla", "facturaSelected"})
+	public void cambiarPantalla(@BindingParam("pantalla") int pantalla) {
+		
+		Arrays.fill(this.pantalla, false);
+		
+		this.pantalla[pantalla] = true;
+		
+		if (pantalla == 1) {
+			
+			this.facturaSelected = new Factura();
+			this.agregarDetalle();
+			
+		}
+		
+	}
+	
+	//======Seccion nueva Factura =====
+	
+	@Command
+	public void onChangeCondicion(@BindingParam("id") String id) {
+		
+			String condStr = id.replace("btn", "").toLowerCase();
+			
+			Clients.evalJavaScript(
+					"document.getElementById('btnContado').classList.toggle('active', '"+condStr+"'=== 'contado');\n"
+					+ "document.getElementById('btnCredito').classList.toggle('active', '"+condStr+"'=== 'credito');\n"
+					+ "document.getElementById('creditoFields').classList.toggle('show', '"+condStr+"' === 'credito');");
+		
+	}
+	
+	@Command
+	public void onChangeMoneda(@BindingParam("id") String id) {
+		
+		String moneda = id.replace("btn", "").toUpperCase();
+		
+		Clients.evalJavaScript(
+				" document.getElementById('btnPyg').classList.toggle('active', '"+moneda+"' === 'PYG');\n"
+				+ " document.getElementById('btnUsd').classList.toggle('active', '"+moneda+"' === 'USD');"
+				+ " document.getElementById('tipoCambioGroup').classList.toggle('show', '"+moneda+"' === 'USD');");
+		
+	}
+	
+	
+	@Command
+	public void agregarDetalle() {
+		
+		this.facturaSelected.getDetalles().add(new FacturaDetalle(this.facturaSelected, 10, 1L,100, this.getCurrentEmpresa()));
+		BindUtils.postNotifyChange(null, null, this.facturaSelected, "detalles");
+	}
+	
+	@Command
+	@NotifyChange({"iva5","iva10","exento","facturaSelected"})
+	public void borrarDetalle(@BindingParam("data") FacturaDetalle detalle) {
+		
+		this.facturaSelected.getDetalles().remove(detalle);
+
+		this.calcularTotales();
+		
+	}
+	
+	protected Empresa getCurrentEmpresa() {
+		
+		Empresa out = new Empresa();
+		out.setEmpresaid((Long) Sessions.getCurrent().getAttribute("empresaid"));
+		
+		return out;
+		//return this.getCurrentEmpresaUsuario().getEmpresa();
+	}
+	
+	private double iva5 = 0;
+	private double iva10 = 0;
+	private double iva0 = 0;
+	
+	@Command
+	@NotifyChange({"iva5","iva10","exento","facturaSelected"})
+	public void calcularTotales() {
+		
+		double totalDetalle = 0;
+		
+		for (FacturaDetalle d : this.facturaSelected.getDetalles()) {
+			
+			double totalLine = d.getCantidad()*d.getPrecioUnitario();
+			
+			 switch ((int) d.getTasaIva()) {
+	         case 10 -> iva10 += totalLine / 11;
+	         case 5  -> iva5  += totalLine / 21;
+	         case 0  -> iva0  += totalLine;
+		 }
+		 
+		 totalDetalle += totalLine;
+			
+		}
+		
+		this.facturaSelected.setTotalDetalle(totalDetalle);
+	}
 
 	public List<Object[]> getFacturaciones() {
 		return facturaciones;
@@ -192,6 +301,46 @@ public class FacturacionVM{
 
 	public void setFiltro(String filtro) {
 		this.filtro = filtro;
+	}
+
+	public Boolean[] getPantalla() {
+		return pantalla;
+	}
+
+	public void setPantalla(Boolean[] pantalla) {
+		this.pantalla = pantalla;
+	}
+
+	public Factura getFacturaSelected() {
+		return facturaSelected;
+	}
+
+	public void setFacturaSelected(Factura facturaSelected) {
+		this.facturaSelected = facturaSelected;
+	}
+
+	public double getIva5() {
+		return iva5;
+	}
+
+	public void setIva5(double iva5) {
+		this.iva5 = iva5;
+	}
+
+	public double getIva10() {
+		return iva10;
+	}
+
+	public void setIva10(double iva10) {
+		this.iva10 = iva10;
+	}
+
+	public double getIva0() {
+		return iva0;
+	}
+
+	public void setIva0(double iva0) {
+		this.iva0 = iva0;
 	}
 	
 	
